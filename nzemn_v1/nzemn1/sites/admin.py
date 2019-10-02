c14ac00139617de86d56a91ebaf16066ec450713
jeffcnz <jeffjcooke@hotmail.com>
2019-10-02 19:05:06 +1300

    Admin Lat Long not map

--- nzemn_v1/nzemn1/sites/admin.py
+++ nzemn_v1/nzemn1/sites/admin.py
@@ -1,4 +1,10 @@
-from django.contrib.gis import admin
+#from django.contrib.gis import admin
+
+from django.contrib import admin
+
+from django.forms.widgets import Textarea
+from django.contrib.gis.db import models
+from . import widgets
 
 from .models import Site, Agency, IdentifierType, SiteAgency, SiteOperation, SiteIdentifiers
 
@@ -18,18 +24,23 @@ class IdentifiersInline(admin.StackedInline):
     model = SiteIdentifiers
     extra = 0
 
-
-class SiteAdmin(admin.OSMGeoAdmin):
+class SiteAdmin(admin.ModelAdmin):
+#class SiteAdmin(admin.OSMGeoAdmin):
     #default_lon = 173.2840
     #default_lat = -41.2706
-    default_lon = 19289886.64
-    default_lat = -5052337.41
+    #default_lon = 19289886.64
+    #default_lat = -5052337.41
 
-    default_zoom = 6
+    #default_zoom = 6
+    formfield_overrides = {
+        #models.PointField: {'widget': Textarea}
+        models.PointField: {'widget': widgets.LatLongWidget}
+    }
 
     fieldsets = [
         (None,               {'fields': ['site_name']}),
-        ('Location', {'fields': ['location'], 'classes': ['collapse']}),
+        #('Location', {'fields': ['location'], 'classes': ['collapse']}),
+        ('Location', {'fields': ['location']}),
     ]
     inlines = [IdentifiersInline, AgenciesInline, OperationInline]
 
