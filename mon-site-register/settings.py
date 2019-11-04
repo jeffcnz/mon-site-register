837d5241e11a6d8cf90cb4775bc2768f849e4466
jeffcnz <jeffjcooke@hotmail.com>
2019-11-04 21:17:00 +1300

    More Heroku GIS troubleshooting

--- mon-site-register/settings.py
+++ mon-site-register/settings.py
@@ -172,11 +172,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # For Heroku
-#GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-#GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
-GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
+#GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
+#GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
 
 # Leaflet Configurations
 LEAFLET_CONFIG = {
